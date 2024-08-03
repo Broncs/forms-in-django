96703797ba4a019039b0e160ee1789a4f19e26d5
jhow <jhowfrombrazil@gmail.com>
2024-08-03 15:54:37 -0300

    sessions

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,7 @@ urlpatterns =[
     path("",views.ReviewView.as_view()),
     path("thank-you",views.Thankyou.as_view()),
     path("reviews",views.ReviewsListView.as_view()),
+    path("reviews/favorite", views.AddFavoriteView.as_view()),
     path("reviews/<int:pk>",views.ReviewDetailView.as_view(), name="single-review"),
     
 ]
