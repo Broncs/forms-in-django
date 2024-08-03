2d96e4c9d42395b25b115067e4983509b28ba1a8
jhow <jhowfrombrazil@gmail.com>
2024-08-03 12:28:35 -0300

    close look at views

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path("",views.ReviewView.as_view()),
-    path("thank-you",views.thank_you)
+    path("thank-you",views.Thankyou.as_view()),
+    path("reviews",views.ReviewsListView.as_view()),
+    path("reviews/<int:pk>",views.ReviewDetailView.as_view(), name="single-review"),
+    
 ]
